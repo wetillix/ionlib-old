e7e05610be05d3ed4f0ec573382b942ec3b22aa3
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2021-03-01 20:17:41 +0100

    👌 IMPROVE (update): update ionic-ui-kit module

---
+++ projects/ionic-ui-kit/src/lib/ionic-ui-kit.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { UiButtonModule } from './components/ui-button/ui-button.module';
+
+@NgModule({
+  declarations: [],
+  imports: [IonicModule.forRoot()],
+  exports: [],
+})
+export class IonicUiKitModule {}
