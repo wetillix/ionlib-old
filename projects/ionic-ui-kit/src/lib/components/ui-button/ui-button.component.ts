e41f282d066c4a675cdd66bf5e1d7f8574082dee
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2021-06-13 13:15:56 +0100

    ✅  Update ui-button component

--- projects/ionic-ui-kit/src/lib/components/ui-button/ui-button.component.ts
+++ projects/ionic-ui-kit/src/lib/components/ui-button/ui-button.component.ts
@@ -1,15 +1,23 @@
+import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ui-button',
   templateUrl: './ui-button.component.html',
-  styleUrls: ['./ui-button.component.css']
+  styleUrls: ['./ui-button.component.css'],
 })
 export class UiButtonComponent implements OnInit {
+  @Input() expand: string;
+  @Input() fill: string;
+  @Input() shape: string;
+  @Input() spinnerName: string;
+  @Input() isShowingSpinner: boolean = true;
+  @Output() click = new EventEmitter<any>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick(event: any) {
+    this.click.emit(event);
   }
-
 }
