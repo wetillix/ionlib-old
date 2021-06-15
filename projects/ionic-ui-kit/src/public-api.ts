72a80b6769f5552e94d7cdefba8367b1044d7502
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2021-06-15 10:33:17 +0100

    👌 IMPROVE (update): update public-api.ts

--- projects/ionic-ui-kit/src/public-api.ts
+++ projects/ionic-ui-kit/src/public-api.ts
@@ -3,4 +3,4 @@
  */
 
 export * from './lib/ionic-ui-kit.module';
-export * from './lib/components/ui-button/ui-button.module';
+export * from './lib/components/ionic-ui-button/ionic-ui-button.module';
