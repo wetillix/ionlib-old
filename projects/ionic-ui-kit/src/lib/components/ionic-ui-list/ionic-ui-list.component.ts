200cd3fdfb63814d49557853c2142b3ed8d5908c
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2021-06-15 10:29:20 +0100

    📦 NEW (add): ionic-ui-list component

---
+++ projects/ionic-ui-kit/src/lib/components/ionic-ui-list/ionic-ui-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ion-ui-list',
+  templateUrl: './ionic-ui-list.component.html',
+  styleUrls: ['./ionic-ui-list.component.css']
+})
+export class IonicUiListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ionic-ui-kit/src/lib/components/ionic-ui-list/ionic-ui-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicUiListComponent } from './ionic-ui-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    IonicUiListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IonicUiListModule { }
