b9ef5e491e6da61c00256d2e960f51cdec74d205
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2021-06-15 10:28:51 +0100

    👌 IMPROVE (update):  ionic-ui-button component

---
+++ projects/ionic-ui-kit/src/lib/components/ionic-ui-button/ionic-ui-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'ion-ui-button',
+  templateUrl: './ionic-ui-button.component.html',
+  styleUrls: ['./ionic-ui-button.component.css'],
+})
+export class IonicUiButtonComponent implements OnInit {
+  @Input() borderRadius: string;
+  @Input() width: string = '90%';
+  @Input() height: string;
+  @Input() color: string;
+  @Input() disabled: boolean = false;
+  @Input() mode: string = 'ios';
+  @Input() buttonText: string = 'Button';
+
+  @Input() spinnerName: string = 'bubbles';
+  @Input() spinnerColor: string;
+  @Input() displaySpinner: string = 'none';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
