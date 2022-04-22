4d2040ccb5118781874f33243e289e714a20abd9
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2022-04-22 17:52:13 +0100

    ✅  Update public-api.ts

--- projects/ionic-ui-kit/src/public-api.ts
+++ projects/ionic-ui-kit/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/ionic-ui-kit.module';
 export * from './lib/components/ionic-ui-button/ionic-ui-button.module';
 export * from './lib/components/ionic-ui-list/ionic-ui-list.module';
 export * from './lib/components/ionic-ui-checkbox/ionic-ui-checkbox.module';
+export * from './lib/components/ionic-ui-input/ionic-ui-input.module';
