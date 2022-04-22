93f0bb1e6248ec89a0ec0c47c31ecebac733cc71
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2022-04-22 17:52:26 +0100

    ✅  Update ionic-ui-kit module

--- projects/ionic-ui-kit/src/lib/ionic-ui-kit.module.ts
+++ projects/ionic-ui-kit/src/lib/ionic-ui-kit.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { IonicUiButtonModule } from './components/ionic-ui-button/ionic-ui-button.module';
 import { IonicUiCheckboxModule } from './components/ionic-ui-checkbox/ionic-ui-checkbox.module';
+import { IonicUiInputModule } from './components/ionic-ui-input/ionic-ui-input.module';
 import { IonicUiListModule } from './components/ionic-ui-list/ionic-ui-list.module';
 
 @NgModule({
   declarations: [],
   imports: [IonicModule.forRoot()],
-  exports: [IonicUiButtonModule, IonicUiListModule, IonicUiCheckboxModule],
+  exports: [
+    IonicUiButtonModule,
+    IonicUiListModule,
+    IonicUiCheckboxModule,
+    IonicUiInputModule,
+  ],
 })
 export class IonicUiKitModule {}
