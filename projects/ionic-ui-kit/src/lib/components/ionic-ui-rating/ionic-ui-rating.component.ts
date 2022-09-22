009604de6896e5eb314ee2d70f96915d3e20c960
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2022-09-22 16:42:02 +0100

    ✅ Update ionic-ui-rating component

---
+++ projects/ionic-ui-kit/src/lib/components/ionic-ui-rating/ionic-ui-rating.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ionic-ui-rating',
+  templateUrl: './ionic-ui-rating.component.html',
+  styleUrls: ['./ionic-ui-rating.component.css']
+})
+export class IonicUiRatingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
